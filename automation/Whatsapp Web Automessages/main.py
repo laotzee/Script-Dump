6aa6c4379df9fcfe89bae60f0750f363d468393b
laotzee <laotzeabreu@proton.me>
2025-07-23 13:55:59 -0400

    Create main.py to implemente modules functionality

---
+++ automation/Whatsapp Web Automessages/main.py
@@ -0,0 +1,16 @@
+from dataManager import increase_counter, read_counter, get_data
+from messenger import send_message
+
+def send_next_message(contact_name):
+
+    counter = read_counter()
+    today_data = get_data(counter)
+
+    if today_data is None:
+        print("data file has no entry for today's counter")
+        raise Exception
+    send_message(
+        contact_name=contact_name,
+        message=today_data,
+    )
+    increase_counter()

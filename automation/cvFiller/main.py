57ef497c6dd5bf9d994a48d9d2d5e014af9eca33
laotzee <laotzeabreu@proton.me>
2026-04-15 15:29:58 -0400

    Fix formatting

--- automation/cvFiller/main.py
+++ automation/cvFiller/main.py
@@ -3,27 +3,32 @@ from odfdo import Document
 import subprocess
 import os
 
+
 def makeFolder(output_dir):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
+
 
 def convert_to_pdf(input_file, output_dir):
     command = [
         "soffice",
         "--headless",
-        "--convert-to", "pdf",
+        "--convert-to",
+        "pdf",
         input_file,
-        "--outdir", output_dir
+        "--outdir",
+        output_dir,
     ]
 
     try:
         result = subprocess.run(command, check=True, capture_output=True, text=True)
         print("Conversion successful!")
         print(result.stdout)
-        
+
     except subprocess.CalledProcessError as e:
         print(f"Error during conversion: {e}")
         print(f"Stderr: {e.stderr}")
+
 
 odt_name = "cover_letter.odt"
 
@@ -35,7 +40,7 @@ country = input("Country you are applying to: ").rstrip()
 folder_name = company.replace(" ", "_")
 makeFolder(folder_name)
 
-doc = Document('template.odt')
+doc = Document("template.odt")
 body = doc.body
 
 body.replace("{{date}}", date)
@@ -43,5 +48,5 @@ body.replace("{{role}}", role)
 body.replace("{{company}}", company)
 body.replace("{{country}}", country)
 
-doc.save(f'{folder_name}/{odt_name}')
+doc.save(f"{folder_name}/{odt_name}")
 convert_to_pdf(f"{folder_name}/{odt_name}", folder_name)
